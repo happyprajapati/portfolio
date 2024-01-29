ecb4ea8baef627db013fed894e3c2565f4cbcf10
Happy <104614758+happyprajapati@users.noreply.github.com>
2024-01-29 19:37:15 +0530

    edit config file

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    rewrites: async () => [
-      {
-        source: "/projects/analog-clock",
-        destination: "/mini-project/Clock/index.html",
-      },
-      {
-        source: "/projects/digital-clock",
-        destination: "/mini-project/Clock1/index.html",
-      },
-      {
-        source: "/projects/parallax-scrolling",
-        destination: "/mini-project/Parallax Scrolling/index.html",
-      },
-      {
-        source: "/projects/calculator",
-        destination: "/mini-project/Calculator/index.html",
-      },
-    ],
 };
 
 module.exports = nextConfig
