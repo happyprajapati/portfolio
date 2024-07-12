58c05d7953986fa8e5dff8df3de0da5ce7c3a2e0
Happy <104614758+happyprajapati@users.noreply.github.com>
2024-07-12 13:04:19 +0530

    final commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Happy | Portfolio",
+  title: "Happy Prajapati| Portfolio",
   description: "Created by Happy Prajapati",
 };
 
